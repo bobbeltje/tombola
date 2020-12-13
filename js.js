2e74ec662b2928d9eeed96de96958671e792a058
bobbeltje <matthiaswubs@gmail.com>
2020-12-13 18:57:05 +0100

    landing page with multiple decks

--- js.js
+++ js.js
@@ -1,3 +1,8 @@
+decks = {
+    'Cat': 1,
+    'Dog': 2,
+    'Mouse': 3
+}
 function make_card(m, id, idx){
     var card = '';
     idx = 3 * (idx - 1);
@@ -25,4 +30,19 @@ function make_number(x){
 }
 function set_card_width(val){
     $('.card-container').css('width', val+'vw');
+}
+function go_tombola(){
+    var x = decks[$('#deck-dropdown').attr('choice')];
+    if (x == undefined) return;
+    $.getJSON('www/' + x + '.json', function load_cards(data) {
+      // globalThis.cards = data;
+      make_card(data, 'card1', 1);
+      make_card(data, 'card2', 2);
+      make_card(data, 'card3', 3);
+      make_card(data, 'card4', 4);
+      make_card(data, 'card5', 5);
+      make_card(data, 'card6', 6);
+      $('#landing').css('display', 'none');
+      $('#tombola').css('display', 'inline-block');
+    });
 }
